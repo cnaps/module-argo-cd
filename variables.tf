35cd817fc0244cb0c4008aeaa8047a9f8025fda9
cnaps <scant10@gmail.com>
2023-03-13 11:28:50 +0900

    ArgoCD module init

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "kubernetes_cluster_cert_data" {
+  type = string
+}
+variable "kubernetes_cluster_endpoint" {
+  type = string
+}
+variable "kubernetes_cluster_name" {
+  type = string
+}
+
+variable "kubernetes_id" {
+  type = string
+}
+
+variable "eks_nodegroup_id" {
+  type = string
+}
